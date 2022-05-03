362bff449404f5ec404d082b8db4d3b6f3135cda
Alex Wulf <alex.f.wulf@gmail.com>
2022-05-03 16:14:33 +0200

    Dependency to google fonts removed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,17 +7,6 @@
         <title>ESW Waschmaschinen</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <script src="{{ asset(('js/app.js')) }}" defer></script>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-        <script type="text/javascript">
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "bqn7m5lz9d");
-        </script>
-
     </head>
     <body class="bg-gray-100 p-5 min-h-screen">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
